d6b1eeae4618abf8031199827c21f54b4cfabdfb
rashel9255 <ibrahim2000167@gmail.com>
2025-09-06 04:12:28 +0600

    handle errors, empty lessons and better ux

--- script.js
+++ script.js
@@ -15,13 +15,23 @@ const displayLevelWord = (words) => {
     const wordContainer = document.getElementById('word-container');
     wordContainer.innerHTML = "";
 
+    if(words.length === 0){
+        wordContainer.innerHTML = `
+            <div class="text-center col-span-full py-10 rounded-xl space-y-6 font-bangla">
+            <img class="mx-auto" src="assets/alert-error.png">
+            <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="text-4xl font-bold">নেক্সট Lesson এ যান</h2>
+        </div>
+        `;
+    }
+
     words.forEach(word => {
         const card = document.createElement('div');
         card.innerHTML = `
             <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "Word is not found"}</h2>
             <p class="font-semibold">Meaning/Pronounciation</p>
-            <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "Meaning is not found"} / ${word.pronunciation ? word.pronunciation : "Pronounciation is not found"}</div>
             <div class="flex justify-between items-center">
                 <button type="button" class="btn bg-[#1A91FF1A] hover:bg-[#1A91FF]" aria-label="More information"><i class="fa-solid fa-circle-info"></i></button>
                 <button type="button" class="btn bg-[#1A91FF1A] hover:bg-[#1A91FF]" aria-label="More information"><i class="fa-solid fa-volume-high"></i></button>
